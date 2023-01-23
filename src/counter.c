f58603363d506e1e45946d974129873268be6ad2
HoseaNA <110534062+HoseaNA@users.noreply.github.com>
2023-01-23 15:47:40 +0700

    update to cpp (c sucks)

--- src/counter.c
+++ src/counter.c
@@ -1,4 +1,4 @@
-float combination1(float numbers[4],char ops[3])
+float combination(float numbers[4],char ops[3], int comb)
 {
     /* All Possibilities
     ((A B) C) D
@@ -8,8 +8,14 @@ float combination1(float numbers[4],char ops[3])
     (A B) (C D)
     */
 
-   /*((A B) C) D*/
-   return count(count(count(numbers[0],numbers[1],ops[0]),numbers[2],ops[1]),numbers[3],ops[2]);
+    if (comb == 1) /*((A B) C) D*/
+    {
+        return count(count(count(numbers[0],numbers[1],ops[0]),numbers[2],ops[1]),numbers[3],ops[2]);
+    }
+    else if (comb == 2) /*(A (B C)) D*/
+    {
+        return count(count(numbers[0],count(numbers[1],numbers[2],ops[1]),ops[0]),numbers[3],ops[2]);
+    }
 }
 
 float count(float num1, float num2, char op)
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,63 @@
+#include<iostream>
+#include<map>
+using namespace std;
+
+bool mainMenu()
+{
+    int choice;
+    cout << "Ketik 1 untuk input manual\nKetik 2 untuk input random\n";
+    cin >> choice;
+
+    float numbers[4];
+    if (choice == 1)
+    {
+        numsManual(numbers);
+    }
+    else if (choice == 2)
+    {
+
+    }
+}
+
+void numsManual(float numbers[4])
+{
+    map<string, float> HashInput;
+    HashInput["2"] = 2.0;
+    HashInput["3"] = 3.0;
+    HashInput["4"] = 4.0;
+    HashInput["5"] = 5.0;
+    HashInput["6"] = 6.0;
+    HashInput["7"] = 7.0;
+    HashInput["8"] = 8.0;
+    HashInput["9"] = 9.0;
+    HashInput["10"] = 10.0;
+    HashInput["A"] = 1.0;
+    HashInput["J"] = 11.0;
+    HashInput["Q"] = 12.0;
+    HashInput["K"] = 13.0;
+
+    cout << "Masukkan 4 angka atau huruf :\n(A, 2-10, J, Q, K)\n";
+    for (int i = 0; i < 4; i++)
+    {
+        string input;
+        cin >> input;
+
+        while (HashInput.find(input) == HashInput.end())
+        {
+            cout << "Input tidak sesuai, silahkan input perbaikan sebanyak input yang tidak sesuai\n";
+            cin >> input;
+        }
+        numbers[i] = HashInput[input];
+    }
+}
+
+void numsRandom(float numbers[])
+{
+    
+}
+
 int main()
 {
     
+    return 0;
 }
